7b089a5e9281fef529240080632dfd40239d6411
Isa Omarzada <isaomarzada@yahoo.com>
2018-12-20 13:48:42 -0600

    checkpoint-auudio-playback: error

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,8 +11,37 @@ class Album extends Component {
      });
 
      this.state = {
-       album: album
+       album: album,
+       currentSong: album.songs[0],
+       isPlaying: false
      };
+
+     this.audioElement = document.createElement('audio');
+     this.audioElement.src = album.songs[0].audioSrc
+   }
+
+   play() {
+     this.audioElement.play();
+     this.setState({ isPlaying: true })
+   }
+
+   pause() {
+     this.audioElement.pause();
+     this.setState({ isPlaying: false })
+   }
+
+   setSong(song) {
+     this.audioElement.src = song.audioSrc;
+     this.setState({ currentSong: song });
+   }
+
+   handleSongClick(song) {
+     const isSameSong = this.state.currentSong === song;
+     if (this.state.isPlaying && isSameSong) {
+       this.pause();
+     } else {
+       this.play()
+     }
    }
 
    render() {
@@ -33,6 +62,16 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>
            <tbody>
+            {this.state.album.songs.map((song, index) =>
+            <tr onMouseEnter={() => this.hoverOn(index)}
+                onMouseLeave={() => this.hoverOff()}
+                className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                  <td>{this.handleHover(song,index)}</td>
+                  <td>{song.title}</td>
+                  <td>{song.duration}</td>
+                  </tr>
+                )
+              }
            </tbody>
          </table>
         </section>
