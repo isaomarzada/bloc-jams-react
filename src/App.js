9f094a066882d99eac50bead37da81f51b259a14
Isa Omarzada <isaomarzada@yahoo.com>
2018-12-27 18:14:53 -0600

    styling error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom'
 import './App.css';
+import './myapp.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
@@ -11,10 +12,10 @@ class App extends Component {
       <div className="App">
         <header>
           <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
+            <Link to='/' id="l1">Home</Link>
+            <Link to='/library' id="l2">Library</Link>
           </nav>
-          <h1>Bloc Jams</h1>
+          <h1 id='title'>Bloc Jams</h1>
         </header>
         <main>
         <Route exact path="/" component={Landing} />
